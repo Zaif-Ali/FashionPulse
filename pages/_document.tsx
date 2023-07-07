3dc7560eaa872121bd3456acd0929679dcb1bb64
Huzaifa <zaifali785@gmail.com>
2023-07-08 03:49:37 +0500

    Favicon Updated

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        {/* Add the link tag for the favicon */}
+        <link rel="icon" href="/images/logo.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import HeroCarousel from "@/components/Carousel/HeroCarousel";
 import ImageGrid from "@/components/ImageGrid";
 import Intouch from "@/components/Intouch";
-import Wrapper from "@/components/Wrapper";
 import { Menimages, Watchimages, Womenimages } from "@/constants/ImageGrid";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Head from "next/head";
 import { useEffect } from "react";
 
 export default function Home() {
@@ -12,44 +12,65 @@ export default function Home() {
     AOS.init({ duration: 2000 });
   }, []);
   return (
-    <main className="py-20 md:px-10">
-      <div data-aos="fade-up">
-        <HeroCarousel />
-      </div>
-      <div className="pt-16" data-aos="fade-up">
-        <header className="text-center">
-          <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
-            Collection for Womens
-          </h2>
-        </header>
-      </div>
-      <div data-aos="fade-up">
-        {/* position 1 mean left to right and 0 mean right to left  */}
-        <ImageGrid data={Womenimages} position={1} />
-      </div>
-      <div className="pt-16" data-aos="fade-up">
-        <header className="text-center">
-          <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
-            Collection for Mens
-          </h2>
-        </header>
-      </div>
-      <div data-aos="fade-up">
-        <ImageGrid data={Menimages} position={0} />
-      </div>
-      <div className="pt-16" data-aos="fade-up">
-        <header className="text-center">
-          <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
-            Watches For Everyone
-          </h2>
-        </header>
-      </div>
-      <div data-aos="fade-up">
-        <ImageGrid data={Watchimages} position={0} straight={true} />
-      </div>
-      <div data-aos="fade-up">
-        <Intouch />
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>{` Home || FashionPulse `}</title>
+        <meta name="keywords" content="titla, meta, nextjs" />
+        <meta name="author" content="ZAIF ALI" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content={`Meta description for the Home page`}
+        />
+      </Head>
+      <main className="py-20 md:px-10">
+        <div data-aos="fade-up">
+          <HeroCarousel />
+        </div>
+        <div className="pt-16" data-aos="fade-up">
+          <header className="text-center">
+            <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
+              Collection for{" "}
+              <span className="text-primary underline underline-offset-4 decoration-primary-foreground/95  ">
+                Womens
+              </span>
+            </h2>
+          </header>
+        </div>
+        <div data-aos="fade-up">
+          {/* position 1 mean left to right and 0 mean right to left  */}
+          <ImageGrid data={Womenimages} position={1} />
+        </div>
+        <div className="pt-16" data-aos="fade-up">
+          <header className="text-center">
+            <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
+              Collection for{" "}
+              <span className="text-primary underline underline-offset-4 decoration-primary-foreground/95 ">
+                Mens
+              </span>
+            </h2>
+          </header>
+        </div>
+        <div data-aos="fade-up">
+          <ImageGrid data={Menimages} position={0} />
+        </div>
+        <div className="pt-16" data-aos="fade-up">
+          <header className="text-center">
+            <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
+              <span className="text-primary underline underline-offset-4 decoration-primary-foreground/95 ">
+                Watches
+              </span>{" "}
+              For Everyone
+            </h2>
+          </header>
+        </div>
+        <div data-aos="fade-up">
+          <ImageGrid data={Watchimages} position={0} straight={true} />
+        </div>
+        <div data-aos="fade-up">
+          <Intouch />
+        </div>
+      </main>
+    </>
   );
 }
